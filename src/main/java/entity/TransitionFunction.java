61e1c705ccc6eb9a60b5d28ec9909c0d8cf7ce88
nyaancat <udal.ol@yandex.ru>
2017-11-23 08:52:43 +0400

    Automat rename added TF rename

--- src/main/java/entity/TransitionFunction.java
+++ src/main/java/entity/TransitionFunction.java
@@ -3,21 +3,21 @@ package entity;
 import java.util.*;
 
 public class TransitionFunction {
-    private Map<String, Map<String, Set<String>>> table; //<startState, <symbol, List<finishState>>>
+    private Map<String, Map<String, Set<String>>> startStateToSymbolAndFinishStates; //<startState, <symbol, List<finishState>>>
 
-    TransitionFunction(Map<String, Map<String, Set<String>>> table){
-        this.table = table;
+    TransitionFunction(Map<String, Map<String, Set<String>>> startStateToSymbolAndFinishStates){
+        this.startStateToSymbolAndFinishStates = startStateToSymbolAndFinishStates;
     }
 
     public TransitionFunction() {
-        table = new HashMap<String, Map<String, Set<String>>>();
+        startStateToSymbolAndFinishStates = new HashMap<String, Map<String, Set<String>>>();
     }
 
     protected Set<String> transit(Set<String> statesFrom, String symbol) throws Exception{
         Set <String> endStates = new HashSet<String>();
         try {
             for (String state : statesFrom) {
-                endStates.addAll(table.get(state).get(symbol));
+                endStates.addAll(startStateToSymbolAndFinishStates.get(state).get(symbol));
             }
         } catch (Exception e){
             //System.out.println("Illegal symbol");
@@ -27,22 +27,22 @@ public class TransitionFunction {
     }
 
     public void addRow(String statefrom, String symbol, Set<String> result){
-        if (!table.containsKey(statefrom)){
+        if (!startStateToSymbolAndFinishStates.containsKey(statefrom)){
             Map<String, Set<String>> tmp = new HashMap<String, Set<String>>();
             tmp.put(symbol, result);
-            table.put(statefrom, tmp);
+            startStateToSymbolAndFinishStates.put(statefrom, tmp);
         }
-        else if(!table.get(statefrom).containsKey(symbol)){
-            table.get(statefrom).put(symbol, result);
+        else if(!startStateToSymbolAndFinishStates.get(statefrom).containsKey(symbol)){
+            startStateToSymbolAndFinishStates.get(statefrom).put(symbol, result);
         }
     }
-//    protected Map<String, Map<String, String>> getTable() {
-//        return table;
-//    }
-//
-//    protected void setTable(Map<String, Map<String, String>> table) {
-//        this.table = table;
-//    }
+    public Map<String, Map<String, Set<String>>> getStartStateToSymbolAndFinishStatesMap() {
+        return startStateToSymbolAndFinishStates;
+    }
+
+    public void setStartStateToSymbolAndFinishStatesMap(Map<String, Map<String, Set<String>>> startStateToSymbolAndFinishStates) {
+        this.startStateToSymbolAndFinishStates = startStateToSymbolAndFinishStates;
+    }
 
 
 
--- src/main/java/service/AutomatService.java
+++ src/main/java/service/AutomatService.java
@@ -9,41 +9,57 @@ import java.util.Map;
 import java.util.Set;
 
 public class AutomatService {
-//    public static Automat union(Automat automat1, Automat automat2){
-//        Set<String> states = new HashSet<>();
-//
-//        Automat automat = new Automat();
-//        return automat;
-//    }
 
-    private static void renameAutomats(Automat automat, int firstStateNumber){
-        Set<String> states = automat.getStates();
+    public void union(Automat automat1, Automat automat2){
+        Set<String> states = new HashSet<>();
+
+        //Automat automat = new Automat();
+        //return automat;
+    }
+
+    private void renameAutomat(Automat automat, int firstStateNumber){
         Map<String, String> oldNameToNewName = new HashMap<>();
         Set<String> newStates = new HashSet<>();
-        Set<String> newCurrStates = new HashSet<>();
-        Set<String> newFinishStates = new HashSet<>();
-        Set<String> newStartStates = new HashSet<>();
-        TransitionFunction newTransitionFunction;
+        Set<String> newCurrStates;
+        Set<String> newFinishStates;
+        Set<String> newStartStates;
+        TransitionFunction newTransitionFunction = new TransitionFunction();
 
-        for (String state : states){
+        for (String state : automat.getStates()){
             String name = "STATE_" + firstStateNumber;
             oldNameToNewName.put(state, name);
             newStates.add(name);
             firstStateNumber++;
         }
 
-        //automat.setStates(newStates);
-        for (String state : automat.getCurrentStates()) {
-            newCurrStates.add(oldNameToNewName.get(state));
+        newCurrStates = renameSet(automat.getCurrentStates(), oldNameToNewName);
+        newStartStates = renameSet(automat.getStartStates(), oldNameToNewName);
+        newFinishStates = renameSet(automat.getFinishStates(), oldNameToNewName);
+
+        Map<String, Map<String, Set<String>>> startStateToSymbolAndFinishStatesMap =
+                automat.getTransitionFunction().getStartStateToSymbolAndFinishStatesMap();
+
+        for (String startState : startStateToSymbolAndFinishStatesMap.keySet()){
+            for (String symbol : startStateToSymbolAndFinishStatesMap.get(startState).keySet()) {
+                newTransitionFunction.addRow(oldNameToNewName.get(startState),
+                        symbol,
+                renameSet(startStateToSymbolAndFinishStatesMap.get(startState).get(symbol), oldNameToNewName));
+            }
         }
 
-        for (String state : automat.getStartStates()) {
-            newStartStates.add(oldNameToNewName.get(state));
-        }
+        automat.setStates(newStates);
+        automat.setCurrentStates(newCurrStates);
+        automat.setStartStates(newStartStates);
+        automat.setFinishStates(newFinishStates);
+        automat.setTransitionFunction(newTransitionFunction);
 
-        for (String state : automat.getFinishStates()) {
-            newFinishStates.add(oldNameToNewName.get(state));
-        }
     }
 
+    private Set<String> renameSet(Set<String> oldStates, Map<String, String> oldNameToNewName){
+        Set<String> newStates = new HashSet<>();
+        for (String state : oldStates) {
+            newStates.add(oldNameToNewName.get(state));
+        }
+        return newStates;
+    }
 }
